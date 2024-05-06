77a5923c7f986fcca4736ba5dae739177900cbb6
Petros Kitsaras <13155066+TheGamerUrso@users.noreply.github.com>
2024-05-06 14:16:29 +0200

    adding books view and thymeleaf

--- src/main/java/guru/springframework/spring5webapp/repositories/BookRepository.java
+++ src/main/java/guru/springframework/spring5webapp/repositories/BookRepository.java
@@ -1,8 +1,7 @@
 package guru.springframework.spring5webapp.repositories;
 
-import org.springframework.data.repository.CrudRepository;
-
 import guru.springframework.spring5webapp.domain.Book;
+import org.springframework.data.repository.CrudRepository;
 
 public interface BookRepository extends CrudRepository<Book, Long> {
 
